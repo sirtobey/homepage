f9baacee4c92f21b74855025a2f699a5ce1a08bc
Tobias Feller <tobiasmichaelfeller@gmail.com>
2022-10-05 22:04:54 +0200

    feat(): add reducer base

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import { FaBeer } from "react-icons/fa";
+import { useReducer } from "react";
+import { FaPlusCircle } from "react-icons/fa";
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { Segment } from "./components/segment/Segment";
 
+const initialState = {
+  segments: [{ title: "Title" }, { title: "Title2" }],
+};
+
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "addSegment":
+      return { segments: [...state.segments, action.segment] };
+    default:
+      throw new Error("Oppps no action found");
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="content">
       <Header />
       <main className="columns">
-        <Segment />
-        <Segment />
+        {state.segments.map((segment) => (
+          <Segment key={segment.title} />
+        ))}
+        <button
+          onClick={() =>
+            dispatch({ type: "addSegment", segment: { title: "new title" } })
+          }
+        >
+          <FaPlusCircle />
+        </button>
       </main>
     </div>
   );
